237310ddcbef8b6ab71117ab11a9af1563d2eb22
vaishakhpuriya-alt <vaishakhpuriya@gmail.com>
2026-02-16 09:49:08 -0800

    Update testcase2.py

--- testcase2.py
+++ testcase2.py
@@ -1,11 +1,9 @@
 import numpy as np
 
 # Create two 2D arrays
-arr1 = np.array([[1, 2, 3],
-                 [4, 5, 6]])
+arr1 = np.array([[1, 2, 3]])
 
-arr2 = np.array([[7, 8, 9],
-                 [1, 2, 3]])
+arr2 = np.array([[7, 8, 9]])
 
 print("Array 1:")
 print(arr1)
