01da52444e331ecba8060672cd747462ace55af3
vaishakhpuriya-alt <vaishakhpuriya@gmail.com>
2026-02-16 09:49:35 -0800

    Update testcase2.py

--- testcase2.py
+++ testcase2.py
@@ -1,6 +1,5 @@
 import numpy as np
 
-# Create two 2D arrays
 arr1 = np.array([[1, 2, 3]])
 
 arr2 = np.array([[7, 8, 9]])
@@ -10,8 +9,6 @@ print(arr1)
 
 print("\nArray 2:")
 print(arr2)
-
-# Element-wise multiplication
 result = arr1 * arr2
 
 print("\nElement-wise Multiplication Result:")
