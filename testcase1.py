038967601cbe219ee3efa9128677f9a9e91a2eb6
vaishakhpuriya-alt <vaishakhpuriya@gmail.com>
2026-02-16 09:49:51 -0800

    Update testcase1.py

--- testcase1.py
+++ testcase1.py
@@ -1,6 +1,5 @@
 import numpy as np
 
-# Create 3x3 array
 arr = np.array([[1, 2, 3],
                 [4, 5, 6],
                 [7, 8, 9]])
@@ -8,7 +7,6 @@ arr = np.array([[1, 2, 3],
 print("Original 3x3 Array:")
 print(arr)
 
-# Reshape to (1,9)
 reshaped_arr = arr.reshape(1, 9)
 
 print("\nReshaped Array (1x9):")
